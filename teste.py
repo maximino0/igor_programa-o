890c4d9959a3b348a7d21e33736b96d34c441896
Maximino <128611506+maximino0@users.noreply.github.com>
2024-08-16 11:57:18 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,19 +1,17 @@
-from veiculos import *
-tipo=input("escolha o tipo do seu veiculo:\nC para Carro:\nM para moto:\n")
-marca=input("qual a marca do seu veiculo:\n")
-ano=int(input("Qual o ano do seu veiculo:\n"))
-preco=float(input("Qual o preço do veiculo:\n"))
-if(ano>=1 and preco>0):
-    if(tipo=="C" or tipo=="c"):
-        modelo=input("qual o modelo do seu carro:\n")
-        potencia=int(input("qual a potencia do seu carro:\n"))
-        Carro=carro(marca,ano,preco,modelo,potencia)
-        print(Carro.exibir_info())
-    elif(tipo=="M" or tipo=="m"):
-        cilindrada=int(input("qual a cilindrada da sua moto:\n"))
-        Moto=moto(marca,ano,preco,cilindrada)
-        print(Moto.exibir_info())
-    else:
-        print("você escolheu o tipo errado de veiculo:")
-else:
-    print("vocẽ colocou numeros invalidos")
+def add_json(p1):
+    add={"nome":input("nome: "),"Idade":input("idade: "),"Prontuário":input("Prontuário: "),"Portugues nota":input("Nota de portugues: "),"Matemática nota":input("Nota de matematica: "),"Ciencias nota":input("Nota de ciencias: "),"media":0,}
+    p1.append(add)
+def media_json(p1,id):
+    n1=int(p1[id]["Portugues nota"])
+    n2=int(p1[id]["Matemática nota"])
+    n3=int(p1[id]["Ciencias nota"])
+    p1[id]["media"]=(n1+n2+n3)/3
+def exibir_json(p1):
+    return print(p1)
+p1=[{"nome":"Igor","Idade":"20 anos","Prontuário":"SP12345","Portugues nota":6,"Matemática nota":8,"Ciencias nota":10,"media":0,},
+    {"nome":"Ana","Idade":"19 anos","Prontuário":"SP45678","Portugues nota":10,"Matemática nota":3,"Ciencias nota":9,"media":0,}]
+add_json(p1)
+for id in range(len(p1)): 
+    media_json(p1,id)
+for id in range(len(p1)): 
+    print(p1[id])
